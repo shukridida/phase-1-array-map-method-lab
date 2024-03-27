2945a5651e64236701e52df6102fe3692f7ca5e1
shukri <didashukri0@gmail.com>
2024-03-27 14:45:55 +0300

    added a file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +13,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased () {
+
+  return tutorials.map(string => {
+    const words = string.split(" ");
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+      });
+    return capitalizedWords.join(" ")
+});
 }
+titleCased()
